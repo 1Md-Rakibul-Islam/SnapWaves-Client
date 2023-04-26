6a42942ac927c24a6cc1ad28e86b4f27eda51fb7
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-04-27 03:13:50 +0600

    Feture card added

---
+++ src/Component/FeturePostCard/FeturePostCard.jsx
@@ -0,0 +1,28 @@
+import { Avatar, Box, Card, Typography } from '@mui/material';
+import React from 'react';
+
+const FeturePostCard = () => {
+    return (
+        <Box sx={{
+            minWidth: '110px',
+            minHeight: '180px',
+            p: 1.5,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            borderRadius: '15px',
+            bgcolor: 'white'
+
+            }}>
+            <Avatar>
+                <img src='' alt='' />
+            </Avatar>
+            <Typography fontSize={13}>
+                Rakibul Islam
+            </Typography>
+        </Box>
+    );
+};
+
+export default FeturePostCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 import RightSideNav from '../../Component/RightSideNav/RightSideNav';
 import PostCard from '../../Component/PostCard/PostCard';
 import CreatePost from '../../Component/CreatePost/CreatePost';
+import FeturePostCard from '../../Component/FeturePostCard/FeturePostCard';
 
 const Home = () => {
     return (
         <Container sx={{ minHeight: '100vh', my: 5 }}>
         <Box sx={{ display: 'flex', alignItems: 'stretch', columnGap: 3.5, my: 5 }} >
             <Box sx={{ display: { lg: "block", xs: "none" }, backgroundColor: "white", minWidth: "240px", padding: "5px", borderRadius: "15px", height: "80vh" }} >
-                {/* <SideBarNavProfile /> */}
                 <CardHeader
                 avatar={
                 <Avatar sx={{ bgcolor: 'blue' }} aria-label="recipe">
@@ -24,10 +24,15 @@ const Home = () => {
                 <RightSideNav />
             </Box>
             <Box sx={{ width: "100%", overflowY: "scroll", height: "80vh", "& > *": { marginTop: "1.25rem", marginBottom: "1.25rem"}}}>
-                {/* { */}
+                    <Box sx={{display: 'flex', columnGap: 2, overflowY: "scroll",}}>
+                        <FeturePostCard></FeturePostCard>
+                        <FeturePostCard></FeturePostCard>
+                        <FeturePostCard></FeturePostCard>
+                        <FeturePostCard></FeturePostCard>
+                        <FeturePostCard></FeturePostCard>
+                    </Box>
                     <CreatePost />
                     <PostCard />
-                {/* } */}
                 <Box sx={{ display: 'flex', justifyContent: 'end', mt: 4 }} >
                     <nav aria-label="Page navigation">
 
